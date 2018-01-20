1c4b7e9393aeaab1fa26271541a96f0a0fa6770a
Marcin Lewandowski <marcin@ravendb.net>
2018-01-20 18:23:21 +0100

    ravendb stub

---
+++ ravendb/project.clj
@@ -0,0 +1,9 @@
+(defproject jepsen.ravendb "0.1.0-SNAPSHOT"
+  :description "A Jepsen test for RavenDB"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main jepsen.ravendb
+  :jvm-opts ["-Dcom.sun.management.jmxremote"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jepsen "0.1.7-SNAPSHOT"]
+                 [net.ravendb/ravendb-client "4.0.0-rc1"]])
